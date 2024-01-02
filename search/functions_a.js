e5661ed9bdb1f36c2a5839427b48e5ff12ce3fcd
brichards64 <brichards64@users.noreply.github.com>
2024-01-02 15:37:00 +0000

    deploy: 91ad23fb2c49dcc673a59ec30e6219c8732e723e

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['log_112',['Log',['../classLogging.html#af7839ee68729b066da269cc012b1fcc9',1,'Logging::Log()'],['../classTool.html#ac65a2dfa2e3531fe8e05fe4112846fcf',1,'Tool::Log()']]],
+  ['log_112',['Log',['../classLogging.html#af7839ee68729b066da269cc012b1fcc9',1,'Logging::Log()'],['../classTool.html#a73b465acc3acbe532d6fe0ba5087dc29',1,'Tool::Log()']]],
   ['logging_113',['Logging',['../classLogging.html#a7a3c6bcf0b0241779e7f500e29162e5e',1,'Logging']]]
 ];
