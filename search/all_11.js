1d55dc85d7dd67b06b00096d0d4ebd8e4bba0a45
brichards64 <brichards64@users.noreply.github.com>
2024-01-06 23:43:35 +0000

    deploy: cb058fa38accdb4e092d888efacd690312d752e4

--- search/all_11.js
+++ search/all_11.js
@@ -7,7 +7,7 @@ var searchData=
   ['threadname_61',['ThreadName',['../structThread__args.html#a594f831be7ce66015a7606e023b24cf4',1,'Thread_args']]],
   ['threads_62',['Threads',['../classUtilities.html#a26ebd7308ea895452c4c1066dc5834e5',1,'Utilities']]],
   ['tool_63',['Tool',['../classTool.html',1,'']]],
-  ['toolchain_64',['ToolChain',['../classToolChain.html',1,'ToolChain'],['../classToolChain.html#a133e224899a743ee4a679396f8569bf9',1,'ToolChain::ToolChain(std::string configfile, int argc=0, char *argv[]=0)'],['../classToolChain.html#adefb544d73c43c2621f3ccb14ad93dc7',1,'ToolChain::ToolChain(int verbose=1, int errorlevel=0, bool log_interactive=true, bool log_local=false, std::string log_local_path=&quot;./log&quot;, bool log_split_files=false, DataModel *in_data_model=0)']]],
+  ['toolchain_64',['ToolChain',['../classToolChain.html',1,'ToolChain'],['../classToolChain.html#a133e224899a743ee4a679396f8569bf9',1,'ToolChain::ToolChain(std::string configfile, int argc=0, char *argv[]=0)'],['../classToolChain.html#a5dc90f42dfd81bbf04b29ff426a79f9c',1,'ToolChain::ToolChain(int verbose, int errorlevel=0, bool log_interactive=true, bool log_local=false, std::string log_local_path=&quot;./log&quot;, bool log_split_files=false, DataModel *in_data_model=0)']]],
   ['toolchainargs_65',['ToolChainargs',['../structToolChainargs.html',1,'']]],
   ['type_66',['Type',['../classBStore.html#a1a2e9beec6992ed4f7ef1c01c211c955',1,'BStore']]]
 ];
--- search/all_b.js
+++ search/all_b.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['log_24',['Log',['../classDataModel.html#aa777da4c632e4659ee5b1447ad513458',1,'DataModel::Log()'],['../classLogging.html#af7839ee68729b066da269cc012b1fcc9',1,'Logging::Log()'],['../classTool.html#a73b465acc3acbe532d6fe0ba5087dc29',1,'Tool::Log()']]],
-  ['logging_25',['Logging',['../classLogging.html',1,'Logging'],['../classLogging.html#a7a3c6bcf0b0241779e7f500e29162e5e',1,'Logging::Logging()']]]
+  ['logging_25',['Logging',['../classLogging.html',1,'Logging'],['../classLogging.html#ae1d1dbc6e252f4b56a6d185578faef27',1,'Logging::Logging()']]]
 ];
