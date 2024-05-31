c62f92709aa43dfe177283a53218ab1eac240093
brichards64 <brichards64@users.noreply.github.com>
2024-05-31 03:51:44 +0000

    deploy: e9d1935e9e651e34b601934382d838340ea37164

--- search/all_10.js
+++ search/all_10.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['serialisableobject_53',['SerialisableObject',['../classToolFramework_1_1SerialisableObject.html',1,'ToolFramework']]],
   ['serialise_54',['Serialise',['../classToolFramework_1_1BStore.html#aad69a5bed1adbf77c47bff31b3509880',1,'ToolFramework::BStore']]],
-  ['set_55',['Set',['../classToolFramework_1_1BStore.html#a69211f9087b585b34fee5818c7e0f67b',1,'ToolFramework::BStore::Set(std::string name, T &amp;in)'],['../classToolFramework_1_1BStore.html#a0e0ec34501857cca62a9c52e2115a953',1,'ToolFramework::BStore::Set(std::string name, T *in, bool persist=true)'],['../classToolFramework_1_1Store.html#acbdc2f9391b5a9ce57d7126b45ffb200',1,'ToolFramework::Store::Set()']]],
+  ['set_55',['Set',['../classToolFramework_1_1BStore.html#a69211f9087b585b34fee5818c7e0f67b',1,'ToolFramework::BStore::Set(std::string name, T &amp;in)'],['../classToolFramework_1_1BStore.html#a0e0ec34501857cca62a9c52e2115a953',1,'ToolFramework::BStore::Set(std::string name, T *in, bool persist=true)'],['../classToolFramework_1_1Store.html#acbdc2f9391b5a9ce57d7126b45ffb200',1,'ToolFramework::Store::Set(std::string name, T in)'],['../classToolFramework_1_1Store.html#af5c02078af68cf5d47bd7851c4e6888a',1,'ToolFramework::Store::Set(std::string name, std::vector&lt; T &gt; in)']]],
   ['store_56',['Store',['../classToolFramework_1_1Store.html',1,'ToolFramework']]],
   ['stores_57',['Stores',['../classToolFramework_1_1DataModelBase.html#ae6dcda92cb7872e42b696c9afaf5f7f5',1,'ToolFramework::DataModelBase']]]
 ];
--- search/functions_f.js
+++ search/functions_f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['serialise_129',['Serialise',['../classToolFramework_1_1BStore.html#aad69a5bed1adbf77c47bff31b3509880',1,'ToolFramework::BStore']]],
-  ['set_130',['Set',['../classToolFramework_1_1BStore.html#a69211f9087b585b34fee5818c7e0f67b',1,'ToolFramework::BStore::Set(std::string name, T &amp;in)'],['../classToolFramework_1_1BStore.html#a0e0ec34501857cca62a9c52e2115a953',1,'ToolFramework::BStore::Set(std::string name, T *in, bool persist=true)'],['../classToolFramework_1_1Store.html#acbdc2f9391b5a9ce57d7126b45ffb200',1,'ToolFramework::Store::Set()']]]
+  ['set_130',['Set',['../classToolFramework_1_1BStore.html#a69211f9087b585b34fee5818c7e0f67b',1,'ToolFramework::BStore::Set(std::string name, T &amp;in)'],['../classToolFramework_1_1BStore.html#a0e0ec34501857cca62a9c52e2115a953',1,'ToolFramework::BStore::Set(std::string name, T *in, bool persist=true)'],['../classToolFramework_1_1Store.html#acbdc2f9391b5a9ce57d7126b45ffb200',1,'ToolFramework::Store::Set(std::string name, T in)'],['../classToolFramework_1_1Store.html#af5c02078af68cf5d47bd7851c4e6888a',1,'ToolFramework::Store::Set(std::string name, std::vector&lt; T &gt; in)']]]
 ];
