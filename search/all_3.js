316b9dcd42a8223fa30d455b46126667b19eb230
brichards64 <brichards64@users.noreply.github.com>
2024-12-13 15:27:16 +0000

    deploy: 01d76a10a28b756393fe8571a8f737ea7127ea9f

--- search/all_3.js
+++ search/all_3.js
@@ -5,6 +5,6 @@ var searchData=
   ['datamodel_12',['DataModel',['../classDataModel.html',1,'DataModel'],['../classDataModel.html#abff03aef2cb531142a35781bb87c3365',1,'DataModel::DataModel()']]],
   ['datamodelbase_13',['DataModelBase',['../classToolFramework_1_1DataModelBase.html#a36b75174d88a96823aa1e650b9033fa6',1,'ToolFramework::DataModelBase::DataModelBase()'],['../classToolFramework_1_1DataModelBase.html',1,'ToolFramework::DataModelBase']]],
   ['datamodelthread_5fargs_14',['DataModelThread_args',['../structDataModelThread__args.html',1,'']]],
-  ['delete_15',['Delete',['../classToolFramework_1_1Store.html#a7fce0f8f3ec7978c5e7cd3d7053f899b',1,'ToolFramework::Store::Delete()'],['../classToolFramework_1_1BStore.html#a4fb9274504aeb81dd3f5feb87af26fb1',1,'ToolFramework::BStore::Delete()']]],
+  ['delete_15',['Delete',['../classToolFramework_1_1Store.html#ab3b0490f4b8e239e3b010fd6d8945a7e',1,'ToolFramework::Store::Delete()'],['../classToolFramework_1_1BStore.html#a4fb9274504aeb81dd3f5feb87af26fb1',1,'ToolFramework::BStore::Delete()']]],
   ['dummytool_16',['DummyTool',['../classDummyTool.html#a33914471b4de346168aa92b5febb6f9c',1,'DummyTool::DummyTool()'],['../classDummyTool.html',1,'DummyTool']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['datamodel_139',['DataModel',['../classDataModel.html#abff03aef2cb531142a35781bb87c3365',1,'DataModel']]],
   ['datamodelbase_140',['DataModelBase',['../classToolFramework_1_1DataModelBase.html#a36b75174d88a96823aa1e650b9033fa6',1,'ToolFramework::DataModelBase']]],
-  ['delete_141',['Delete',['../classToolFramework_1_1BStore.html#a4fb9274504aeb81dd3f5feb87af26fb1',1,'ToolFramework::BStore::Delete()'],['../classToolFramework_1_1Store.html#a7fce0f8f3ec7978c5e7cd3d7053f899b',1,'ToolFramework::Store::Delete()']]],
+  ['delete_141',['Delete',['../classToolFramework_1_1BStore.html#a4fb9274504aeb81dd3f5feb87af26fb1',1,'ToolFramework::BStore::Delete()'],['../classToolFramework_1_1Store.html#ab3b0490f4b8e239e3b010fd6d8945a7e',1,'ToolFramework::Store::Delete()']]],
   ['dummytool_142',['DummyTool',['../classDummyTool.html#a33914471b4de346168aa92b5febb6f9c',1,'DummyTool']]]
 ];
