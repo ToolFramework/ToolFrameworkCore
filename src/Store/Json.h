fc8a27b572ca70cf190adf88bbaf5378fa74f30c
Evgenii Zhemchugov <evgenii.zhemchugov@warwick.ac.uk>
2025-04-30 11:58:08 +0100

    Implement json_encode for pointers and std::array
    Testing revealed difficulties regarding extending json_encode with
    user-defined types. In the following chain of calls:
    
        struct S { ... };
        json_encode(std::string,  std::vector<std::array<S, 6>>); // 1
        json_encode(std::ostream, std::vector<std::array<S, 6>>); // 2
        json_encode(std::ostream, std::array<S, 6>*, ...); // 3
        json_encode(std::ostream, std::array<S, 6>); // 4
        json_encode(std::ostream, S*); // 5
        json_encode(std::ostream, S); // 6, implemented in the user code
    
    the compiler might only be able to find the user function if it was
    called with unqualified names and argument-dependent lookup took
    action. Also, without forward declarations provided by this patch the
    compiler could not reach step 4 since json_encode for array has been
    declared after json_encode for pointers. This suggests possible problems
    with user and other STL containers. See
    
    https://stackoverflow.com/questions/28187170/c-enforce-second-pass-name-lookup-in-template-function
    https://akrzemi1.wordpress.com/2015/11/19/overload-resolution/
    
    for the discussion.
    
    One possible solution is to declare
    
        template <typename T> void json_encode(std::ostream&, const T&);
    
    and use template specializations instead of overloaded functions, but it
    needs special casing to exclude arithmetic types implemented by a
    different template --- and possibly it may clash in a similar way with
    other user defined types.

--- src/Store/Json.h
+++ src/Store/Json.h
@@ -10,6 +10,15 @@
 
 namespace ToolFramework {
 
+template <typename T, size_t N>
+bool json_encode(std::ostream&, const std::array<T, N>&);
+
+template <typename T>
+bool json_encode(std::ostream&, const std::vector<T>&);
+
+template <typename T>
+bool json_encode(std::ostream&, const std::map<std::string, T>&);
+
 template <typename T>
 typename std::enable_if<std::is_arithmetic<T>::value, bool>::type
 json_encode(std::ostream& output, T datum) {
@@ -21,19 +30,28 @@ bool json_encode(std::ostream& output, const char* datum);
 bool json_encode(std::ostream& output, const std::string& datum);
 
 template <typename T>
-bool json_encode(std::ostream& output, const std::vector<T>& data) {
+bool json_encode(std::ostream& output, const T* data, size_t size) {
   output << '[';
   bool comma = false;
-  for (const T& datum : data) {
+  for (size_t i = 0; i < size; ++i) {
     if (comma)
       output << ',';
-    else
-      comma = true;
-    if (!json_encode(output, datum)) return false;
+    comma = true;
+    if (!json_encode(output, data[i])) return false;
   };
   output << ']';
   return true;
-}
+};
+
+template <typename T, size_t N>
+bool json_encode(std::ostream& output, const std::array<T, N>& array) {
+  return json_encode(output, array.data(), array.size());
+};
+
+template <typename T>
+bool json_encode(std::ostream& output, const std::vector<T>& vector) {
+  return json_encode(output, vector.data(), vector.size());
+};
 
 template <typename T>
 bool json_encode(std::ostream& output, const std::map<std::string, T>& data) {
