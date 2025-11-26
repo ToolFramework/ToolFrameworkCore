a3da323c86b98077d6cdcaf8ee7ff33d1353f3b7
brichards64 <benjamin.richards@warwick.ac.uk>
2025-11-26 15:34:31 +0000

    corected makefule paths, added pool quota options, and added mem tool draft

--- src/DataModelBase/Pool.h
+++ src/DataModelBase/Pool.h
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <Utilities.h>
 #include <chrono>
+#include <atomic>
 
 namespace ToolFramework{
 
@@ -21,6 +22,9 @@ namespace ToolFramework{
     std::chrono::high_resolution_clock::time_point now;
     std::chrono::high_resolution_clock::time_point managing_timer;
     uint64_t count;
+    unsigned int* sum;
+    unsigned int* counter;
+    
   };
   
   
@@ -28,11 +32,15 @@ namespace ToolFramework{
     
   public:
     
-    Pool(bool in_manage=false, uint16_t period_ms=1000, size_t in_object_cap=1){
-      
+    Pool(bool in_manage=true, size_t in_object_cap=1, uint16_t period_ms=1000){
+
+      counter = 0;
+      sum = 0;
       manage=in_manage;
       args.manage_period_ms=period_ms;
       args.object_cap=in_object_cap;
+      args.sum = &sum;
+      args.counter = &counter;
       
       if(manage) m_utils.CreateThread("pool_manager", &Thread, &args);
       
@@ -41,10 +49,20 @@ namespace ToolFramework{
     ~Pool(){
       if(manage) m_utils.KillThread(&args);
       Clear();
-    }    
+    }
+
+    void SetPeriod(uint16_t period_ms){
+      args.manage_period_ms=period_ms;
+    }
+
+    void SetObjectCap(size_t in_object_cap){
+       args.object_cap=in_object_cap;
+    }
     
     template <typename... Args> T* GetNew(Args... in_args){
       mtx.lock();
+      counter++;
+      sum+=objects.size();      
       if(objects.size()>0){
 	T* tmp=objects.front();
 	objects.pop();
@@ -53,6 +71,7 @@ namespace ToolFramework{
 
       }
       mtx.unlock();
+      
       return new T(in_args...);
     }
     
@@ -76,13 +95,17 @@ namespace ToolFramework{
       Pool_args<T>* args = reinterpret_cast<Pool_args<T>*>(arg);
       args->count = std::chrono::duration<double, std::milli>( std::chrono::high_resolution_clock::now() - args->managing_timer).count();
       if(args->count < args->manage_period_ms){
-	usleep((args->manage_period_ms - args->count)/1000);
+	usleep((args->manage_period_ms - args->count)*1000);
 	return;
       }
       args->mtx->lock();
-      while(args->objects->size() > args->object_cap){
-	delete args->objects->front();
-	args->objects->pop();
+      if( *args->counter!=0 && (((*args->sum) / (*args->counter)) >  args->object_cap)){
+	while(args->objects->size() > args->object_cap){
+	  delete args->objects->front();
+	  args->objects->pop();
+	}
+	*args->counter = 0;
+	*args->sum = 0;
       }
       args->mtx->unlock();
       args->managing_timer=std::chrono::high_resolution_clock::now();
@@ -96,6 +119,8 @@ namespace ToolFramework{
     Utilities m_utils;
     Pool_args<T> args;
     bool manage;
+    unsigned int counter;
+    unsigned int sum;
     
   };
   
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,27 @@
 #include <string>
+//#include "mem.hpp"
 #include "ToolChain.h"
 #include "DataModel.h"
 //#include "DummyTool.h"
 
 using namespace ToolFramework;
 
+
 int main(int argc, char* argv[]){
 
+  //memory_usage.PrintUsage("init");
   std::string config_file;
-  if (argc==1)config_file="configfiles/Dummy/ToolChainConfig";
+  if (argc==1) config_file="configfiles/Dummy/ToolChainConfig";
   else config_file=argv[1];
+  //memory_usage.PrintUsage("after config");
 
+  
   DataModel* data_model = new DataModel();
+  //memory_usage.PrintUsage("after DM");
+
   ToolChain tools(config_file, data_model, argc, argv);
-  
-  
+  //memory_usage.PrintUsage("after tc");
+
   //DummyTool dummytool;    
   
   //tools.Add("DummyTool",&dummytool,"configfiles/DummyToolConfig");
@@ -26,7 +33,6 @@ int main(int argc, char* argv[]){
   //  tools.Initialise();
   // tools.Execute();
   //tools.Finalise();
-  
   
   return 0;
   
